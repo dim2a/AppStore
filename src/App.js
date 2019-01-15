6a1f7a465a7f0ccc8130698dc34aa2d08cdbcb2e
dim2a <buinicki.dim@gmail.com>
2019-01-15 13:02:10 +0300

    Сделан скелет, все пока захардкожено(

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+      <div className={classes.wrapper}>
+        <header className={classes.header}>          
+          <a href="/" className={classes.logo}>
+            <img src='https://vignette.wikia.nocookie.net/reckoning/images/e/e4/EA_Games_logo.png/revision/latest?cb=20110527201122' alt='logo' />
           </a>
+          <a href="/" className={classes.user}>
+          <img src='http://icons.iconarchive.com/icons/icons8/ios7/256/Users-User-Male-4-icon.png' alt='user' />
+          </a>          
         </header>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <a href="/">Головоломки</a>
+            </li>
+            <li>
+              <a href="/">Гонки</a>
+            </li>
+            <li>
+              <a href="/">Cпорт</a>
+            </li>
+            <li>
+              <a href="/">Карточные</a>
+            </li>
+            <li>
+              <a href="/">Экшен</a>
+            </li>
+            <li>
+              <a href="/">Settings</a>
+            </li>
+          </ul>
+        </nav>
+        <div className={classes.content}>Content</div>
       </div>
     );
   }
