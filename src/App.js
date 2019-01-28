f48932f90cef1208913ddb998d3b55c60b49792a
dim2a <buinicki.dim@gmail.com>
2019-01-28 15:18:59 +0300

    Введен state

--- src/App.js
+++ src/App.js
@@ -5,22 +5,6 @@ import Navigation from './Components/Navigation/Navigation'
 import Content from './Components/Content/Content'
 import Layout from './hoc/Layout/Layout'
 
-let games = {
-  puzzles : [
-    {id: 1, name: 'asd', img: 'https://lh3.googleusercontent.com/MPsMoIDG-sjsSzkAoNZeYPTo0wfjm_AemMNrgChj826Np0UxUoA46sTKfVY58HscO7w=s180', counter:10},
-    {id: 2, name: 'asd', img: 'https://lh3.googleusercontent.com/c7KS4VRehzA6KQ1aNHuhZgM5fdjrTSb0L8mnOL0bpKM6sVTgESjwYe09ns8sFKcFTvQ=s180', counter:5},
-    {id: 3, name: 'asd', img: 'https://lh3.googleusercontent.com/OoAWo-XEztwlvJV6sxxZs5xpso48ZqpossB5Pz1OS2yuUlg16qlPKzkjBKvxrVACGZgP=s180', counter:8},
-    {id: 4, name: 'asd', img: 'https://lh3.googleusercontent.com/jt35uzoKyo-pfqTn04J9DwVumr9tNk2z3CdBOUEJnj1DeygYSq-BzkzV_A8UZy2Bnlv_=s180', counter:22},
-    {id: 5, name: 'asd', img: 'https://lh3.googleusercontent.com/eP-n6dEzIbuFIlhFqI_4cTe9jKciRPdW1XSq2rDf0xgdC-iohdpL8Sl580BhZCwigkM=s180', counter:17},
-    {id: 6, name: 'asd', img: 'https://lh3.googleusercontent.com/CBdTcT0JoukL0VzMLTUDOn_iFKEI_OUwXqRYknzBlUHIHzP8IKjUdCjV9-w2e5tH1xA=s180', counter:46},
-    {id: 6, name: 'asd', img: 'https://lh3.googleusercontent.com/9px8lmdYS9C_tq8RcJhTJQvQJR-66sUXx_c6QY6-RUxJRkry9Dn_ymmv5B1nbpMSGA=s180', counter:58},    
-  ],
-  race : [],
-  sport : [],
-  cards : [],
-  action : []
-}
-
 class App extends Component {
   render() {
     return (
@@ -28,7 +12,7 @@ class App extends Component {
       <Header />
       <Navigation />
       <Layout>
-        <Content games={games} />
+        <Content games={this.props.state.games} />
       </Layout>
       </div>
     )
---
+++ src/Redux/State.js
@@ -0,0 +1,19 @@
+let state = {
+  games : {
+    puzzles : [
+      {id: 1, name: 'asd', img: 'https://lh3.googleusercontent.com/MPsMoIDG-sjsSzkAoNZeYPTo0wfjm_AemMNrgChj826Np0UxUoA46sTKfVY58HscO7w=s180', counter:10},
+      {id: 2, name: 'asd', img: 'https://lh3.googleusercontent.com/c7KS4VRehzA6KQ1aNHuhZgM5fdjrTSb0L8mnOL0bpKM6sVTgESjwYe09ns8sFKcFTvQ=s180', counter:5},
+      {id: 3, name: 'asd', img: 'https://lh3.googleusercontent.com/OoAWo-XEztwlvJV6sxxZs5xpso48ZqpossB5Pz1OS2yuUlg16qlPKzkjBKvxrVACGZgP=s180', counter:8},
+      {id: 4, name: 'asd', img: 'https://lh3.googleusercontent.com/jt35uzoKyo-pfqTn04J9DwVumr9tNk2z3CdBOUEJnj1DeygYSq-BzkzV_A8UZy2Bnlv_=s180', counter:22},
+      {id: 5, name: 'asd', img: 'https://lh3.googleusercontent.com/eP-n6dEzIbuFIlhFqI_4cTe9jKciRPdW1XSq2rDf0xgdC-iohdpL8Sl580BhZCwigkM=s180', counter:17},
+      {id: 6, name: 'asd', img: 'https://lh3.googleusercontent.com/CBdTcT0JoukL0VzMLTUDOn_iFKEI_OUwXqRYknzBlUHIHzP8IKjUdCjV9-w2e5tH1xA=s180', counter:46},
+      {id: 6, name: 'asd', img: 'https://lh3.googleusercontent.com/9px8lmdYS9C_tq8RcJhTJQvQJR-66sUXx_c6QY6-RUxJRkry9Dn_ymmv5B1nbpMSGA=s180', counter:58},    
+    ],
+    race : [],
+    sport : [],
+    cards : [],
+    action : []
+  }
+}
+
+  export default state
